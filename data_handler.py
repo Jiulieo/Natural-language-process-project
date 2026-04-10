373c1e1716ea71c78fe026b5173933475fe7da57
Luca <80696384+Jiulieo@users.noreply.github.com>
2026-04-10 12:06:17 +0200

    Add files via upload

---
+++ data_handler.py
@@ -0,0 +1,43 @@
+import json
+import os
+import random
+
+class DatasetManager():
+    #initialize the manager to point the folder with the example i want to read
+    def __init__(self,task_folder_path):
+        self.file_path = os.path.join(task_folder_path, "task.json")
+        self.data = self.load_data()
+    
+    def load_data(self):
+        #legge il json
+        try:
+            with open(self.file_path, 'r', encoding='utf-8') as f:
+                raw_data = json.load(f)
+            return raw_data.get("examples",[])
+        except FileNotFoundError:
+            print(f"File non trovato: in {self.file_path}")
+            return []
+        
+    def get_random_sample(self):
+        #Pesca un problema a caso e restituisce input e target
+        if not self.data:
+            return None
+        #qui prende dalla lista (caricata tramite json.load) un elemento a caso, che corrisponde ad una coppia input/target
+        sample = random.choice(self.data)
+
+        question = sample.get("input", "")
+        target = sample.get("target_scores", {})
+
+        correct = ""
+        options = []
+
+        for option, p in target.items():
+            options.append(option)
+            if p == 1:
+                correct = option
+
+        return {
+            "input": question,
+            "options": options,
+            "correct_answer": correct
+        }
---
+++ llm_client.py
@@ -0,0 +1,41 @@
+import torch
+from transformers import pipeline
+
+class LLM_interface:
+    #di default carica il modello Qwen 1.8 bilion parameter
+    def __init__(self, model_id = "Qwen/Qwen1.5-1.8B-Chat"):
+        print(f"caricamente del modello {model_id}")
+
+        #se possibile usa la gpu (cuda)
+        self.device = 0 if torch.cuda.is_available else -1
+
+        #con le gpu è conveniente usare float16 perchè è ottimizzato e va più veloce di float32(single precision) anche se più impreciso
+        dtype = torch.float16 if self.device == 0 else torch.float32
+
+        self.pipe = pipeline(
+            "text-generation",
+            model = model_id,
+            device= self.device,
+            torch_dtype = dtype
+        )
+        print("modello caricato")
+
+    #send the prompt (logic problem) to the model and generate an answer
+    def prompt_model(self, prompt, max_new_tokens = 150, temperature = 0.7):
+        #il prompt viene formattato
+        messages = [
+            {"role":"system","content": "Sei un esperto di logica, risolvi il problema ragionando passo per passo"},
+            {"role":"user","content":prompt}
+        ]
+
+        outputs = self.pipe(
+            messages,
+            max_new_tokens = max_new_tokens,
+            temperature = temperature,
+            do_sample = True if temperature>=0 else False
+        )
+
+        #Extract the answer
+        answer = outputs[0]["generated-text"][-1]["content"]
+        return answer.strip()
+
